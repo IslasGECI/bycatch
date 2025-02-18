f544e1784ec47dfb2f49ecda8be12afece00a395
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-18 12:51:57 -0800

    ✅ Pass tests

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -1,3 +1,24 @@
-return_one <- function() {
-  return(1)
+get_trips <- function(data) {
+  dataGroup <- track2KBA::formatFields(
+    dataGroup = data,
+    fieldID   = "track_id",
+    fieldDate = "date_gmt",
+    fieldTime = "time",
+    fieldLon  = "longitude",
+    fieldLat  = "latitude"
+  )
+  colony <- dataGroup |>
+    dplyr::summarise(
+      Longitude = dplyr::first(lon_colony),
+      Latitude  = dplyr::first(lat_colony)
+    )
+  trips <- track2KBA::tripSplit(
+    dataGroup  = dataGroup,
+    colony     = colony,
+    innerBuff  = 3, # kilometers
+    returnBuff = 10,
+    duration   = 1, # hours
+    rmNonTrip  = TRUE
+  )
+  return(trips)
 }
