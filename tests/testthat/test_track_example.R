08229f6022a0c920b42c9e7394a579663a8721df
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-18 13:56:27 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_track_example.R
+++ tests/testthat/test_track_example.R
@@ -1,7 +1,12 @@
 describe("Get trips from GECI data", {
+  gps_data <- readr::read_csv("/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv", show_col_types = FALSE)
   it("Calculate trips ids", {
-    gps_data <- readr::read_csv("/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv", show_col_types = FALSE)
     obtained <- get_trips(gps_data)
     expect_true("tripID" %in% colnames(obtained@data))
+  })
+  it("Calculate foraging trips list", {
+    obtained <- get_summary_of_trips(gps_data)
+    expected_columns <- c("tripID", "n_locs", "departure", "return", "duration", "total_dist")
+    expect_true(all(expected_columns %in% colnames(obtained)))
   })
 })
