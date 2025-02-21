ed055096f4834a90d69b14410f2ccc6675076d4b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-21 13:17:23 -0800

    🛑🧪 Fail tests

---
+++ tests/testthat/test_cli.R
@@ -0,0 +1,10 @@
+describe("Write trips summary", {
+  it("write_trips_cummary", {
+    gps_path <- "/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv"
+    output_path <- "/workdir/tests/trips_summary.csv"
+    options <- list("data_path" = gps_path, "output_path" = output_path)
+    testtools::if_exist_remove(file_path)
+    write_trips_summary(options)
+    expect_true(exist_output_file(output_path))
+  })
+})
