4effd35d09e41a0e236c9ec6ea7c04d4f53c456f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-21 13:30:44 -0800

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -1,4 +1,5 @@
 write_trips_summary <- function(options) {
   readr::read_csv(options[["data_path"]], show_col_types = TRUE) |>
+    get_summary_of_trips() |>
     readr::write_csv(options[["output_path"]])
 }
