29ea0acd74081bf05d39942ec4ded6926bc92a2d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-21 13:20:36 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -3,7 +3,7 @@ describe("Write trips summary", {
     gps_path <- "/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv"
     output_path <- "/workdir/tests/trips_summary.csv"
     options <- list("data_path" = gps_path, "output_path" = output_path)
-    testtools::if_exist_remove(file_path)
+    testtools::if_exist_remove(output_path)
     write_trips_summary(options)
     expect_true(testtools::exist_output_file(output_path))
   })
