f7e074b63f5fc9cf6af13e6901626f54ed1d46fb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 11:00:29 -0700

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -11,3 +11,10 @@ write_trips <- function(options, config_content) {
   trips@data |>
     readr::write_csv(options[["output_path"]])
 }
+get_domain_specific_options <- function() {
+  data_path <- geci.optparse::character_option(c("-i", "--data_path"), default = "/workdir/reports/tables/input.csv", help = "File path of the desire input")
+  config_path <- geci.optparse::character_option(c("-c", "--config_path"), default = "/workdir/reports/non-tabular/config_file.json", help = "File path of the configuration")
+  output_path <- geci.optparse::character_option(c("-o", "--output_path"), default = "/workdir/reports/tables/result.csv", help = "File path of the desire output")
+  option_names <- c(data_path, config_path, output_path)
+  geci.optparse::get_options_from_vec(option_names)
+}
