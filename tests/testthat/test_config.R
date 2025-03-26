25b4143f0022c31c1930327d0dc780e0fb9b9995
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 09:47:50 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_config.R
+++ tests/testthat/test_config.R
@@ -1,8 +1,12 @@
 describe("read configuration file", {
-  it("Read configuration file", {
-    config_path <- "/workdir/tests/data/trips_config.json"
-    obtained <- read_config(config_path)
+  config_path <- "/workdir/tests/data/trips_config.json"
+  obtained <- read_config(config_path)
+  it("Expected parameters", {
     expected_keys <- c("inner_buff", "return_buff", "duration", "colony")
     expect_true(all(expected_keys %in% names(obtained)))
+  })
+  it("colony structure", {
+    obtained_colony_df <- names(obtained$colony)
+    expect_true(tibble::is_tibble(obtained_colony_df))
   })
 })
