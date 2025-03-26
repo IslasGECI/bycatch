8b41fd815a133720a70386efd8ba884bea2c6245
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 09:24:54 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_config.R
+++ tests/testthat/test_config.R
@@ -1,6 +1,6 @@
 describe("read configuration file", {
   config_path <- "tests/data/trips_config.json"
   obtained <- read_config(config_path)
-  expected_keys <- "inner_buff, return_buff, duration"
+  expected_keys <- c("inner_buff", "return_buff", "duration")
   expect_true(expected_keys %in% names(obtained))
 })
