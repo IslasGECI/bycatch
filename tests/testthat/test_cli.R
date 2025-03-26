8c6bee9f28dd31ad3c88d71ccf85f35b0af642f1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 10:49:33 -0700

    ♻️  Refactor

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -1,10 +1,12 @@
+gps_path <- "/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv"
+config_path <- "/workdir/tests/data/trips_config.json"
+
 describe("Write trips summary", {
   it("write_trips_cummary", {
-    gps_path <- "/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv"
     output_path <- "/workdir/tests/trips_summary.csv"
-    options <- list("data_path" = gps_path, "output_path" = output_path)
+    options <- list("data_path" = gps_path, "config_path" = config_path, "output_path" = output_path)
     testtools::if_exist_remove(output_path)
-    write_trips_summary(options)
+    xxwrite_trips_summary(options)
     expect_true(testtools::exist_output_file(output_path))
     obtained <- readr::read_csv(output_path, show_col_types = FALSE)
     expected_columns <- c("tripID", "n_locs", "departure", "return", "duration", "total_dist")
@@ -15,9 +17,7 @@ describe("Write trips summary", {
 
 describe("Write trips geographic points", {
   it("write_trips", {
-    gps_path <- "/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv"
     output_path <- "/workdir/tests/trips_geographic_points.csv"
-    config_path <- "/workdir/tests/data/trips_config.json"
     options <- list("data_path" = gps_path, "config_path" = config_path, "output_path" = output_path)
     testtools::if_exist_remove(output_path)
     write_trips(options)
