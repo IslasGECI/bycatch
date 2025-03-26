416f5b6e39400aacc6e074198a1cd47c2e080672
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 10:15:51 -0700

    ♻️  Refactor

--- R/track_example.R
+++ R/track_example.R
@@ -5,6 +5,30 @@ get_summary_of_trips <- function(gps_data) {
   return(sumTrips)
 }
 
+colony_df <- tibble::tibble(Longitude = -118.29162, Latitude = 28.88421)
+config_content <- list(inner_buff = 3, return_buff = 10, duration = 1, colony = colony_df)
+xxget_trips <- function(data, config_content) {
+  dataGroup <- track2KBA::formatFields(
+    dataGroup = data,
+    fieldID   = "track_id",
+    fieldDate = "date_gmt",
+    fieldTime = "time",
+    fieldLon  = "longitude",
+    fieldLat  = "latitude"
+  )
+  colony <- config_content$colony
+  trips <- track2KBA::tripSplit(
+    dataGroup  = dataGroup,
+    colony     = colony,
+    innerBuff  = config_content$inner_buff, # kilometers
+    returnBuff = config_content$return_buff,
+    duration   = config_content$duration, # hours
+    rmNonTrip  = TRUE
+  )
+  return(trips)
+}
+
+
 get_trips <- function(data) {
   dataGroup <- track2KBA::formatFields(
     dataGroup = data,
