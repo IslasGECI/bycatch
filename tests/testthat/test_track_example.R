95ed24cdeef9be11068f5027cd3e05dd3d813a39
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 10:18:40 -0700

    ♻️  Refactor

--- tests/testthat/test_track_example.R
+++ tests/testthat/test_track_example.R
@@ -1,7 +1,9 @@
 describe("Get trips from GECI data", {
   gps_data <- readr::read_csv("/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv", show_col_types = FALSE)
   it("Calculate trips ids", {
-    obtained <- get_trips(gps_data)
+    colony_df <- tibble::tibble(Longitude = -118.29162, Latitude = 28.88421)
+    config_content <- list(inner_buff = 3, return_buff = 10, duration = 1, colony = colony_df)
+    obtained <- xxget_trips(gps_data, config_content)
     expect_true("tripID" %in% colnames(obtained@data))
   })
   it("Calculate foraging trips list", {
