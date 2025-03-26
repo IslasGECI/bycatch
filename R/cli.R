7332eedeb7142c0edf8ce1b6d4910da3fd6b254b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 10:38:29 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -8,6 +8,10 @@ write_trips_summary <- function(options) {
 write_trips <- function(options) {
   colony_df <- tibble::tibble(Longitude = -118.29162, Latitude = 28.88421)
   config_content <- list(inner_buff = 3, return_buff = 10, duration = 1, colony = colony_df)
+  xxwrite_trips(options, config_content)
+}
+
+xxwrite_trips <- function(options, config_content) {
   trips <- readr::read_csv(options[["data_path"]], show_col_types = TRUE) |>
     get_trips(config_content)
   trips@data |>
