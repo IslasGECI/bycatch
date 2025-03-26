a8a17c3c9e7382251afc5bf36018936591a952fc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 09:50:35 -0700

    ✅ Pass tests

--- R/read_config.R
+++ R/read_config.R
@@ -1,5 +1,5 @@
 read_config <- function(config_path) {
   json_content <- rjson::fromJSON(file = config_path)
-  json_content$colony <- ""
+  json_content$colony <- tibble::tibble()
   return(json_content)
 }
