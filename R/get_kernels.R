c57561ae5037b2b3718a09bd620f9f83f99e51fc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-05 13:39:18 -0700

    ✅ Pass tests

--- R/get_kernels.R
+++ R/get_kernels.R
@@ -1,3 +1,10 @@
 get_scale_parameters <- function(trips, trips_summary) {
-  return(NULL)
+  complete_trips <- subset(trips, trips$Returns == "Yes")
+  tracks <- track2KBA::projectTracks(dataGroup = complete_trips, projType = "azim", custom = TRUE)
+  hVals <- track2KBA::findScale(
+    tracks = tracks,
+    scaleARS = TRUE,
+    sumTrips = trips_summary
+  )
+  return(hVals)
 }
