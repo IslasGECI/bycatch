1a6ed921ddc9a2916da2f18c52ff6624d3c78bf8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-05 14:27:31 -0700

    ✅ Pass tests

--- R/get_kernels.R
+++ R/get_kernels.R
@@ -1,3 +1,21 @@
+get_kernel_density_estimates <- function(gps_data, config_content) {
+  trips <- get_trips(gps_data, config_content)
+  complete_trips <- subset(trips, trips$Returns == "Yes")
+  colony <- config_content$colony
+  sumTrips <- track2KBA::tripSummary(trips = complete_trips, colony = colony)
+  scale_parameters <- get_scale_parameters(complete_trips, sumTrips)
+  tracks <- track2KBA::projectTracks(dataGroup = complete_trips, projType = "azim", custom = TRUE)
+  tracks <- tracks[tracks$ColDist > 3, ] # remove trip start and end points near colony
+
+  KDE <- track2KBA::estSpaceUse(
+    tracks = tracks,
+    scale = scale_parameters$mag,
+    levelUD = 50,
+    polyOut = TRUE
+  )
+  return(KDE)
+}
+
 get_scale_parameters <- function(trips, trips_summary) {
   complete_trips <- subset(trips, trips$Returns == "Yes")
   tracks <- track2KBA::projectTracks(dataGroup = complete_trips, projType = "azim", custom = TRUE)
