55c73a630a03c797c80866f8f5f93e4890d33545
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-05 13:57:14 -0700

    ♻️  Refactor

--- R/get_kernels.R
+++ R/get_kernels.R
@@ -1,10 +1,11 @@
 get_scale_parameters <- function(trips, trips_summary) {
   complete_trips <- subset(trips, trips$Returns == "Yes")
   tracks <- track2KBA::projectTracks(dataGroup = complete_trips, projType = "azim", custom = TRUE)
+  complete_summary <- subset(trips_summary, trips_summary$complete == "complete trip")
   hVals <- track2KBA::findScale(
     tracks = tracks,
     scaleARS = TRUE,
-    sumTrips = trips_summary
+    sumTrips = complete_summary
   )
   return(hVals)
 }
--- tests/testthat/test_track_example.R
+++ tests/testthat/test_track_example.R
@@ -8,6 +8,7 @@ describe("Get trips from GECI data", {
   })
   it("Calculate foraging trips list", {
     obtained <- get_summary_of_trips(gps_data, config_content)
+    print(obtained)
     expected_rows <- 121
     expect_equal(nrow(obtained), expected_rows)
     expected_complete_trips <- 116
