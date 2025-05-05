0e474733941365808f99710e95f51b996af96def
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-05 14:20:01 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_kernels.R
+++ tests/testthat/test_kernels.R
@@ -3,13 +3,18 @@ describe("Calculate space use", {
   colony_df <- tibble::tibble(Longitude = -118.29162, Latitude = 28.88421)
   config_content <- list(inner_buff = 3, return_buff = 10, duration = 1, colony = colony_df)
   trips <- get_trips(gps_data, config_content)
+  trips_summary <- readr::read_csv("/workdir/tests/data/trips_summary.csv", show_col_types = FALSE)
   it("calculates candidate smoothing parameter values ", {
-    trips_summary <- readr::read_csv("/workdir/tests/data/trips_summary.csv", show_col_types = FALSE)
     obtained <- get_scale_parameters(trips, trips_summary)
     print(obtained)
     expected_ncol <- 5
     expect_equal(ncol(obtained), expected_ncol)
     expected_magnitud <- 5.45
     expect_equal(obtained$mag, expected_magnitud)
+  })
+  it("Calculate Kernel Density Estimates", {
+    obtained <- get_kernel_density_estimates(gps_data, config_content)
+    number_of_individuals <- 10
+    expect_equal(nrow(obtained$UDPolygons), number_of_individuals)
   })
 })
