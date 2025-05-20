b4bb02354053ac7ed52a8a4a1fae918d4ba0a7ce
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-20 13:37:28 -0700

    ✅ Pass tests

--- R/representative_assess.R
+++ R/representative_assess.R
@@ -1,7 +1,16 @@
 Track2KBA_Wrapper <- R6::R6Class(
   "Track2KBA_Wrapper",
   public = list(
+    complete_trips = NULL,
+    colony = NULL,
+    trips = NULL,
     initialize = function(gps_data, config_content) {
+      self$trips <- get_trips(gps_data, config_content)
+      self$complete_trips <- subset(self$trips, self$trips$Returns == "Yes")
+      self$colony <- config_content$colony
+    },
+    get_tracks = function() {
+      track2KBA::projectTracks(dataGroup = self$complete_trips, projType = "azim", custom = TRUE)
     }
   )
 )
