7f60c818d6b84a1364c63edc791612f422f0af1a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-05-27 11:48:41 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_representative_assess.R
+++ tests/testthat/test_representative_assess.R
@@ -22,11 +22,15 @@ describe("Check representativity", {
     expected_area <- 44250
     expect_equal(obtained_area, expected_area, tolerance = 1e-3)
   })
+  gps_data <- readr::read_csv("/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv", show_col_types = FALSE)
+  config_content <- list(inner_buff = 3, return_buff = 10, duration = 1, colony = colony_df)
+  obtained <- Track2KBA_Wrapper$new(gps_data, config_content)
+  obtained_repr <- obtained$get_representative_assess(percentage_distribution = 50)
   it("get representative assess with percentage distribution", {
-    gps_data <- readr::read_csv("/workdir/tests/data/bl_gps_albatros_guadalupe_20percent_sample.csv", show_col_types = FALSE)
-    config_content <- list(inner_buff = 3, return_buff = 10, duration = 1, colony = colony_df)
-    obtained <- Track2KBA_Wrapper$new(gps_data, config_content)
-    obtained_assess <- obtained$get_representative_assess(percentage_distribution = 50)
-    expect_true(inherits(obtained_assess, "data.frame"))
+    expect_true(inherits(obtained_repr, "data.frame"))
+  })
+  it("Get potential site", {
+    obtained_site <- obtained$get_site()
+    expect_true(inherits(obtained_site, "sf"))
   })
 })
