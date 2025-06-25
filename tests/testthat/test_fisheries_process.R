4db2373c05926efefeb1c2d3ffdb402b237c21b9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-25 13:08:42 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_fisheries_process.R
+++ tests/testthat/test_fisheries_process.R
@@ -15,4 +15,9 @@ describe("Processes fisheries data", {
     expected_rows <- 1
     expect_equal(nrow(obtained), expected_rows)
   })
+  it("filters fisheries data by date range and latitude/longitude", {
+    obtained <- filter_fisheries_by_date_and_lat_long(fisheries_data, start = "2013-01-01", end = "2013-06-30", lat_min = 123.0, lat_max = 124.0, long_min = -77.0, long_max = -76.0)
+    expected_rows <- 1
+    expect_equal(nrow(obtained), expected_rows)
+  })
 })
