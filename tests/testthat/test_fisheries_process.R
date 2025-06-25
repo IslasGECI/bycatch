0f656c9580a831111832ef2c2b5a1267e0e5b895
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-25 13:03:57 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_fisheries_process.R
+++ tests/testthat/test_fisheries_process.R
@@ -2,11 +2,16 @@ describe("Processes fisheries data", {
   fisheries_data <- tibble::tibble(
     Nombre = c("Test Fishery", "Sample Fishery", "Example Fishery"),
     FechaRecepcionUnitrac = c("2013-05-01T00:00:00Z", "2014-05-01T00:00:00Z", "2014-07-01T00:00:00Z"),
-    Latitud = c(123.456, 123.456, 123.456),
-    Longitud = c(-76.543, -76.543, -76.543)
+    Latitud = c(123.456, 122.456, 125.456),
+    Longitud = c(-76.543, -74.543, -78.543)
   )
   it("filters fisheries data by date range", {
     obtained <- filter_fisheries_by_date(fisheries_data, start = "2014-01-01", end = "2014-06-30")
+    expected_rows <- 1
+    expect_equal(nrow(obtained), expected_rows)
+  })
+  it("filter rows between latitude and longitude", {
+    obtained <- filter_fisheries_by_lat_long(fisheries_data, lat_min = 123.0, lat_max = 124.0, long_min = -77.0, long_max = -76.0)
     expected_rows <- 1
     expect_equal(nrow(obtained), expected_rows)
   })
