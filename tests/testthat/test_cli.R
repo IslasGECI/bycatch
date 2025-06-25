8a163b9a759026df4a1586f1e2fcbb70f9c0a036
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-25 13:48:48 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -81,6 +81,6 @@ describe("Write trips geographic points", {
 })
 describe("Define domain specific options", {
   obtained_options <- get_domain_specific_options()
-  expected_options <- c("data-path", "config-path", "output-path", "percentage-distribution", "n-iterations")
+  expected_options <- c("data-path", "config-path", "output-path", "percentage-distribution", "n-iterations", "start", "end", "lat-min", "lat-max", "lon-min", "lon-max")
   expect_true(all(expected_options %in% names(obtained_options)))
 })
