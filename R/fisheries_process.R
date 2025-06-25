01eb27e2776001470b2314f46a364e964fe3a8f5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-25 13:05:46 -0700

    ✅ Pass tests

--- R/fisheries_process.R
+++ R/fisheries_process.R
@@ -5,3 +5,11 @@ filter_fisheries_by_date <- function(fisheries_data, start, end) {
         as.Date(FechaRecepcionUnitrac) <= as.Date(end)
     )
 }
+
+filter_fisheries_by_lat_long <- function(fisheries_data, lat_min, lat_max, long_min, long_max) {
+  fisheries_data |>
+    dplyr::filter(
+      Latitud >= lat_min & Latitud <= lat_max &
+        Longitud >= long_min & Longitud <= long_max
+    )
+}
