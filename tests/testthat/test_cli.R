f8408103e9c88321bf96a14a0b1b9f583439e44f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-25 13:36:09 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -1,5 +1,23 @@
 config_path <- "/workdir/tests/data/trips_config.json"
 
+describe("process fisheries data", {
+  output_path <- "/workdir/tests/filtered_fisheries_data.csv"
+  fisheries_path <- "/workdir/tests/data/fisheries_data.csv"
+  start <- "2014-01-01"
+  end <- "2014-06-30"
+  lat_min <- 11.87329
+  lat_max <- 32.62694
+  lon_min <- -122.174
+  lon_max <- -92.21958
+  options <- list("data-path" = fisheries_path, "start" = start, "end" = end, "lat-min" = lat_min, "lat-max" = lat_max, "lon-min" = lon_min, "lon-max" = lon_max, "output-path" = output_path)
+  it("write figure", {
+    testtools::if_exist_remove(output_path)
+    process_fisheries_data(options)
+    expect_true(testtools::exist_output_file(output_path))
+    testtools::if_exist_remove(output_path)
+  })
+})
+
 describe("plot ussage area by a proportion of the total birds", {
   output_path <- "/workdir/tests/usage_area.png"
   gps_path <- "/workdir/tests/data/bl_sample_10_percent_5_ids.csv"
