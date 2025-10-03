257f8b75ef587b8f7087a21a60f1944ef5360373
Mario Villasante <mario.villasante@islas.org.mx>
2025-10-03 13:14:23 -0700

    Revert "♻️  Refactor" Only remove the warnings This reverts commit 321cb7e18229dd2158fce3688f59f7e3cb47abc8.

--- R/track_example.R
+++ R/track_example.R
@@ -10,7 +10,7 @@ get_trips <- function(data, config_content) {
     dataGroup = data,
     fieldID = "track_id",
     fieldDate = "date_gmt",
-    fieldTime = "time_gmt",
+    fieldTime = "time",
     fieldLon = "longitude",
     fieldLat = "latitude",
     formatDT = "ymd_HMS"
