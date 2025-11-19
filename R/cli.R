05ea4f768e224298076de6dd8b2529e240f9e5ac
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-11-19 14:41:37 -0800

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -31,6 +31,18 @@ plot_usage_area_by_individual <- function(options) {
   grDevices::dev.off()
 }
 
+plot_potential_site <- function(options) {
+  config_content <- read_config(options[["config-path"]])
+  gps_data <- readr::read_csv(options[["data-path"]], show_col_types = FALSE)
+  percentage_distribution <- options[["percentage-distribution"]]
+  n_iterations <- options[["n-iterations"]]
+
+  wrapper <- Track2KBA_Wrapper$new(gps_data, config_content, percentage_distribution)
+  representative_assess <- wrapper$get_representative_assess(percentage_distribution, n_iterations)
+  site <- wrapper$get_potential_site(representative_assess, percentage_distribution, population_size = options[["population-size"]])
+  track2KBA::mapSite(site)
+  ggplot2::ggsave(filename = options[["output-path"]], device = "png")
+}
 
 #' Plot Representative Assessment
 #'
--- R/representative_assess.R
+++ R/representative_assess.R
@@ -50,6 +50,16 @@ Track2KBA_Wrapper <- R6::R6Class(
         polyOut = FALSE
       )
       return(Site)
+    },
+    get_potential_site = function(repr, percentage_distribution, population_size) {
+      Site <- track2KBA::findSite(
+        KDE = self$KDE$KDE.Surface,
+        represent = repr$out,
+        levelUD = percentage_distribution,
+        popSize = population_size,
+        polyOut = TRUE
+      )
+      return(Site)
     }
   )
 )
