56609f5e38482b8b2cffc0627dacd3c0f7c55fce
Your Name <you@example.com>
2026-05-12 20:51:03 -0700

    🛑🧪🧩🚧 Add failing test for compute_space_use

---
+++ tests/testthat/test_compute.R
@@ -0,0 +1,19 @@
+describe("compute_space_use", {
+  it("returns list with KDE_surface, UDPolygons, colony, and tracks", {
+    sf::sf_use_s2(FALSE)
+    data <- readRDS("/workdir/tests/data/completed_trips.rds")
+    colony_df <- tibble::tibble(Longitude = -118.29162, Latitude = 28.88421)
+    config <- list(colony = colony_df)
+
+    result <- compute_space_use(data, config, levelUD = 50, smoothing_method = "log_median")
+
+    expect_type(result, "list")
+    expect_true(all(c("KDE_surface", "UDPolygons", "colony", "tracks") %in% names(result)))
+    expect_true(inherits(result$KDE_surface, "RasterLayer"))
+    expect_true(inherits(result$UDPolygons, "SpatialPolygonsDataFrame"))
+    expect_true(inherits(result$tracks, "SpatialPointsDataFrame"))
+    expect_s3_class(result$colony, "tbl_df")
+    number_of_individuals <- 3
+    expect_equal(nrow(result$UDPolygons), number_of_individuals)
+  })
+})
